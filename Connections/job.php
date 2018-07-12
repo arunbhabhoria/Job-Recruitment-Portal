8c774fec4ea1d05dd9fb5f675437dc9277a4cce6
Dhairya Chandra <dhairyachandra@outlook.com>
2018-07-12 22:13:50 +0530

    Update job.php

--- Connections/job.php
+++ Connections/job.php
@@ -2,10 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_job = "project6sem-mysqldbserver.mysql.database.azure.com:3306";
+$hostname_job = "localhost";
 $database_job = "job";
-$username_job = "dhairya@project6sem-mysqldbserver";
-$password_job = "Pratyaksh@1442";
-$conn = mysqli_init();
-$job = mysqli_real_connect($conn, $hostname_job, $username_job, $password_job, $database_job, 3306); or trigger_error(mysql_error(),E_USER_ERROR);
+$username_job = "root";
+$password_job = "";
+$job = mysql_pconnect($hostname_job, $username_job, $password_job) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
