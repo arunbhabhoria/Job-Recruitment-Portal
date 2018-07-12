e845e1a1a935dd037a3b7a967277f23605b8bdef
Dhairya Chandra <dhairyachandra@outlook.com>
2018-07-12 11:52:20 +0530

    Azure
    Azure connection

--- Connections/job.php
+++ Connections/job.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_job = "localhost";
+$hostname_job = "6semproject.database.windows.net";
 $database_job = "job";
-$username_job = "root";
+$username_job = "dhairya";
 $password_job = "";
-$job = mysql_pconnect($hostname_job, $username_job, $password_job) or trigger_error(mysql_error(),E_USER_ERROR); 
+$job = mysql_pconnect($hostname_job, $username_job, $password_job) or trigger_error(mysql_error(),E_USER_ERROR);
 ?>
