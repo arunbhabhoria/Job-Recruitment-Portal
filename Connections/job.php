e628f3bc3d794ef31b8497e257f0e233fd3acf45
Dhairya Chandra <dhairyachandra@outlook.com>
2018-07-12 13:32:35 +0530

    initial

--- Connections/job.php
+++ Connections/job.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_job = "6semproject.database.windows.net";
+$hostname_job = "localhost";
 $database_job = "job";
-$username_job = "dhairya";
+$username_job = "root";
 $password_job = "";
 $job = mysql_pconnect($hostname_job, $username_job, $password_job) or trigger_error(mysql_error(),E_USER_ERROR);
 ?>
