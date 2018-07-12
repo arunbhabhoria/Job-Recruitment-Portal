2051b20bc540562676737f194af6d06feb71c0a5
Dhairya Chandra <dhairyachandra@outlook.com>
2018-07-12 21:09:35 +0530

    Update job.php

--- Connections/job.php
+++ Connections/job.php
@@ -2,7 +2,7 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_job = "52.165.163.223/project6sem-mysqldbserver.mysql.database.azure.com:3306";
+$hostname_job = "project6sem-mysqldbserver.mysql.database.azure.com:3306";
 $database_job = "job";
 $username_job = "dhairya@project6sem-mysqldbserver";
 $password_job = "Pratyaksh@1442";
