aaa418cb625989ccc4f598cbea691ea29d33103a
Dhairya Chandra <dhairyachandra@outlook.com>
2018-07-12 13:37:54 +0530

    az

--- Connections/job.php
+++ Connections/job.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_job = "localhost";
+$hostname_job = "127.0.0.1:49885";
 $database_job = "job";
-$username_job = "root";
+$username_job = "azure";
 $password_job = "";
 $job = mysql_pconnect($hostname_job, $username_job, $password_job) or trigger_error(mysql_error(),E_USER_ERROR);
 ?>
